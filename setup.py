0d7a098d15eb7cd27852ef0a265807bebb5118c2
Himanshu Arora <129079320+Himanshu-1703@users.noreply.github.com>
2024-07-21 15:45:09 +0530

    dvc initialized

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project demonstrates how DVC automatically tracks parameters and metrics in experiment tracking when working with pipelines',
+    author='Himanshu',
+    license='MIT',
+)
